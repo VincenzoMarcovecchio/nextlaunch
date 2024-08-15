72eebe24c57bfee1e58e9241a285547386bf3ded
VincenzoMarcovecchio <vinceoldmark@gmail.com>
2024-08-15 11:12:49 +0200

    change

--- server/thespacedevs.ts
+++ server/thespacedevs.ts
@@ -1,45 +1,53 @@
 import Axios from "axios";
-import { updateLaunchLibraryv2DBAgencies, updateLaunchLibraryv2DBLaunches, updateLaunchLibraryv2DBLaunchers } from "./database";
+import {
+	updateLaunchLibraryv2DBAgencies,
+	updateLaunchLibraryv2DBLaunches,
+	updateLaunchLibraryv2DBLaunchers,
+} from "./database";
 
-const LAUNCH_ALL_URL = "https://ll.thespacedevs.com/2.0.0/launch/?format=json&mode=detailed&limit=100";
+const LAUNCH_ALL_URL =
+	"https://ll.thespacedevs.com/2.0.0/launch/?format=json&mode=detailed&limit=100";
 
-const LAUNCH_UPCOMING_URL = "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?format=json&mode=detailed&limit=100";
+const LAUNCH_UPCOMING_URL =
+	"https://ll.thespacedevs.com/2.0.0/launch/upcoming/?format=json&mode=detailed&limit=100";
 
-const AGENCY_URL = "https://ll.thespacedevs.com/2.0.0/agencies/?format=json&mode=detailed&limit=100";
+const AGENCY_URL =
+	"https://ll.thespacedevs.com/2.0.0/agencies/?format=json&mode=detailed&limit=100";
 
-const LAUNCHER_URL = "https://ll.thespacedevs.com/2.0.0/config/launcher/?format=json&mode=detailed&limit=100";
+const LAUNCHER_URL =
+	"https://ll.thespacedevs.com/2.0.0/config/launcher/?format=json&mode=detailed&limit=100";
 
 const TIMEOUT_BETWEEN_CALLS_MS = 7 * 60 * 1000;
 
 export type LaunchLibraryV2Launch = {
-    id: string;
-    launch_library_id: number;
+	id: string;
+	launch_library_id: number;
 
-    name: string;
-    net: string;
-    tbdtime: boolean;
-    tbddate: boolean;
-    //isostart: string,
-    launch_service_provider?: {
-        id: number;
-        //abbrev: string;
-        name: string;
-        url: string | null;
-        //infoURL: string | null;
-        //wikiURL: string | null;
-        country_code: string | null;
-        //countryCode: string | null;
-    };
-    rocket: {
-        id: number;
-        configuration: {
-            id: number;
-            name: string;
-        }
-        
-        //configuration: ...
+	name: string;
+	net: string;
+	tbdtime: boolean;
+	tbddate: boolean;
+	//isostart: string,
+	launch_service_provider?: {
+		id: number;
+		//abbrev: string;
+		name: string;
+		url: string | null;
+		//infoURL: string | null;
+		//wikiURL: string | null;
+		country_code: string | null;
+		//countryCode: string | null;
+	};
+	rocket: {
+		id: number;
+		configuration: {
+			id: number;
+			name: string;
+		};
 
-        /*
+		//configuration: ...
+
+		/*
         name: string;
         infoURLs: string[];
         wikiURL: string | null;
@@ -48,116 +56,121 @@ export type LaunchLibraryV2Launch = {
         agencies: {
             countryCode: string
         }[]*/
-    };
-    pad: {
-        map_url: string;
-    };
-    /*
+	};
+	pad: {
+		map_url: string;
+	};
+	/*
     location: {
         pads: {
             mapURL: string
         }[]
     }
     */
-    mission: {
-        name: string;
-    }[];
-    
-    /*
+	mission: {
+		name: string;
+	}[];
+
+	/*
     missions: {
         name: string;
     }[],
     */
 
-    vidURLs: {
-        url: string
-    }[];
-}
+	vidURLs: {
+		url: string;
+	}[];
+};
 
 export type LaunchLibraryV2Agency = {
-    id: number;
-    abbrev: string;
-    country_code: string;
-    name: string;
-    description: string;
-    info_url: string;
-    wiki_url: string;
-    type: string;
-}
+	id: number;
+	abbrev: string;
+	country_code: string;
+	name: string;
+	description: string;
+	info_url: string;
+	wiki_url: string;
+	type: string;
+};
 
 export type LaunchLibraryV2Launcher = {
-    abbrev: string;
-    country_code: string;
-    info_url: string;
-    wiki_url: string;
-}
+	abbrev: string;
+	country_code: string;
+	info_url: string;
+	wiki_url: string;
+};
 
 type TheSpaceDevResponse<T> = {
-    results: T[];
-    count: number;
-    next?: string;
-    previous: string;
+	results: T[];
+	count: number;
+	next?: string;
+	previous: string;
+};
+
+export async function getLauncLibraryv2Launches(
+	upcoming: boolean
+): Promise<LaunchLibraryV2Launch[]> {
+	let url = upcoming ? LAUNCH_UPCOMING_URL : LAUNCH_ALL_URL;
+
+	return getLauncLibraryV2Rows<LaunchLibraryV2Launch>(url);
 }
 
-export async function getLauncLibraryv2Launches(upcoming: boolean): Promise<LaunchLibraryV2Launch[]> {
-    let url = upcoming ? LAUNCH_UPCOMING_URL : LAUNCH_ALL_URL;
-
-    return getLauncLibraryV2Rows<LaunchLibraryV2Launch>(url);
+export async function getLauncLibraryv2Agencies(): Promise<
+	LaunchLibraryV2Launch[]
+> {
+	return getLauncLibraryV2Rows<any>(AGENCY_URL);
 }
 
-export async function getLauncLibraryv2Agencies(): Promise<LaunchLibraryV2Launch[]> {
-    return getLauncLibraryV2Rows<any>(AGENCY_URL);
-}
-
-export async function getLauncLibraryv2Launchers(): Promise<LaunchLibraryV2Launch[]> {
-    return getLauncLibraryV2Rows<any>(LAUNCHER_URL);
+export async function getLauncLibraryv2Launchers(): Promise<
+	LaunchLibraryV2Launch[]
+> {
+	return getLauncLibraryV2Rows<any>(LAUNCHER_URL);
 }
 
 async function getLauncLibraryV2Rows<T>(url: string): Promise<T[]> {
-    let rows: T[] = [];
+	let rows: T[] = [];
 
-    while(url) {
-        console.log("loading", url);
+	while (url) {
+		console.log("loading", url);
 
-        try {
-            const response = await Axios.get<TheSpaceDevResponse<T>>(url, {
-                headers: {
-                    "user-agent": "nextrocket.space danieleff@gmail.com"
-                }
-            });
+		try {
+			const response = await Axios.get<TheSpaceDevResponse<T>>(url, {
+				headers: {
+					"user-agent": "nextrocket.space danieleff@gmail.com",
+				},
+			});
 
-            await timeoutAsync(TIMEOUT_BETWEEN_CALLS_MS);
+			await timeoutAsync(TIMEOUT_BETWEEN_CALLS_MS);
 
-            rows = rows.concat(response.data.results);
-            url = response.data.next;
-        } catch(e) {
-            console.log(e.response!.data);
-            throw e;
-        }
-    }
+			rows = rows.concat(response.data.results);
+			url = response.data.next;
+		} catch (e) {
+			console.log(e.response!.data);
+			throw e;
+		}
+	}
 
-    return rows;
+	return rows;
 }
 
 export async function timeoutAsync(millis: number) {
-    return new Promise(resolve => setTimeout(resolve, millis));
+	return new Promise((resolve) => setTimeout(resolve, millis));
 }
 
-
 async function load() {
-    const upcoming = await getLauncLibraryv2Launches(false);
-    updateLaunchLibraryv2DBLaunches(upcoming, false);
+	const upcoming = await getLauncLibraryv2Launches(false);
+	updateLaunchLibraryv2DBLaunches(upcoming, false);
 
-    //console.log();
+	//console.log();
 
-    //const agencies = await getLauncLibraryv2Agencies();
-    //updateLaunchLibraryv2DBAgencies(agencies, false);
+	//const agencies = await getLauncLibraryv2Agencies();
+	//updateLaunchLibraryv2DBAgencies(agencies, false);
 
-    //const launchers = await getLauncLibraryv2Launchers();
-    //updateLaunchLibraryv2DBLaunchers(launchers, false);
+	//const launchers = await getLauncLibraryv2Launchers();
+	//updateLaunchLibraryv2DBLaunchers(launchers, false);
 
-    //console.log(upcoming);
-    //console.log(upcoming.length);
+	//console.log(upcoming);
+	//console.log(upcoming.length);
 }
 
 //load();
